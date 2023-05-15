9d2beb9cc8ba92ccfce99a52cc2edb7aaf49347a
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-05-15 17:51:40 +0300

    complete assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+def add(num1,num2)
+    return num1 + num2
+end
+
+sum = add(1,2)
+puts sum
+
+def halve(number)
+    if number.class != Integer
+      return nil
+    end
+  
+    return number/2 
+end
+
+result = halve(4)
+puts result
